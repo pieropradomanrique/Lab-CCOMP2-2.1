f86e7dc404719bcf1abb07718ebcfeca2fdaea63
Piero <pieromanrique@ucsp.edu.pe>
2020-04-02 20:13:26 -0600

    First commit

---
+++ Lab_cout_cin.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int a;
+	string b;
+	cout << "<<<<<<<<<<<<<IMPRIMIR TUS DATOS>>>>>>>>>>>>>";
+	cout << endl;
+	cout << endl;
+	cout << "INGRESE SU NOMBRE: ";
+	cin >> b;
+	cout << "INGRESE SU EDAD: ";
+	cin >> a;
+	cout << "SU NOMBRE ES: " << b << endl;
+	cout << "SU EDAD ES: " << a << endl;
+	cout << "GRACIAS";
+}
